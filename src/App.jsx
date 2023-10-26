1dcf8ed00dbeb46304bfdd1b918afe1c4640a50a
Nithin <nithinraj.dev@gmail.com>
2023-10-26 19:15:17 +0530

    build commit 2

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,21 @@
 import { CssBaseline } from "@mui/material";
-import { BrowserRouter } from "react-router-dom";
+import { BrowserRouter, HashRouter } from "react-router-dom";
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 import Routes from "./application/routes/routes";
-import muiTheme from "./theme"
-import { ThemeProvider} from "@mui/material/styles";
+import muiTheme from "./theme";
+import { ThemeProvider } from "@mui/material/styles";
 
 export default function App() {
   return (
     <>
-      <BrowserRouter>
+      <HashRouter hashType="noslash">
         <ThemeProvider theme={muiTheme}>
           <CssBaseline />
           <Routes />
           <ToastContainer />
         </ThemeProvider>
-      </BrowserRouter>
+      </HashRouter>
     </>
   );
 }
