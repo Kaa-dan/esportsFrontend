d5ebe7b0e8e4212ff2ed9eee7440dafd6d31f516
Nithin <nithinraj.dev@gmail.com>
2023-10-27 09:49:29 +0530

    hosting commit

--- vite.config.js
+++ vite.config.js
@@ -8,7 +8,7 @@ export default defineConfig({
     port: 4000,
     proxy:{
       "/api":{
-        target:"https://localhost:5000",
+        target:process.env.VITE_API_SERVER_URL,
         changeOrigin:true
       }
     }
